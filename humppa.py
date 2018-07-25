6dcdfc11a950673cc29f9873ff7bd485d5fe164c
Raphaël Stefanini <rphl@rphl.net>
2018-07-25 10:54:39 +0300

    Use api domain

--- humppa.py
+++ humppa.py
@@ -5,23 +5,23 @@ import requests
 import subprocess
 
 
-BASE_URL = 'https://www.humppakone.com/'
+BASE_URL = 'https://api.humppakone.com/'
 
 
 class InvalidLogin(Exception):
     pass
 
 
-class Song():
+class Song:
     def __init__(self, data):
         self.api_url = data['url']
         self.id = data['id']
         self.mp3_url = data['download_url']
-        self.filename = data['filename'].encode('utf-8')
+        self.filename = data['filename']
         self.title = re.sub(r'^.*/', '', self.filename)
 
 
-class Player():
+class Player:
     def __init__(self):
         self.token = None
         self.current_song = None
@@ -37,7 +37,7 @@ class Player():
         while True:
             print("\033[H\033[J")
             print('Next: {}'.format(self.current_song.title))
-            command = raw_input('[P]lay / [N]ext / [S]earch / [Q]uit')
+            command = input('[P]lay / [N]ext / [S]earch / [Q]uit')
             if command.lower() == 'p':
                 self.play()
             elif command.lower() == 'n':
@@ -50,9 +50,9 @@ class Player():
 
     def prompt_search(self):
         print("\033[H\033[J")
-        search_terms = raw_input('Search: ')
+        search_terms = input('Search: ')
         r = requests.get(
-            '{}api/songs/?q={}'.format(BASE_URL, search_terms),
+            '{}songs/?q={}'.format(BASE_URL, search_terms),
             headers={
                 'Authorization': 'TOKEN {}'.format(self.token)
             }
@@ -64,7 +64,7 @@ class Player():
         for i, song in enumerate(results):
             print('{}. {}'.format(i+1, song.title))
 
-        n = raw_input('Enter search result number')
+        n = input('Enter search result number')
         try:
             k = int(n)
             self.current_song = results[k-1]
@@ -116,7 +116,7 @@ class Player():
 
 
     def prompt_login(self):
-        username = raw_input("Username (%s): " % getpass.getuser())
+        username = input("Username (%s): " % getpass.getuser())
         if not username:
             username = getpass.getuser()
         password = getpass.getpass()
@@ -124,17 +124,17 @@ class Player():
 
     def get_auth_token(self, username, password):
         r = requests.post(
-            '{}api/auth/login'.format(BASE_URL),
+            '{}login'.format(BASE_URL),
             data={'username': username,
                   'password': password}
         )
-        if r.status_code == 400:
+        if r.status_code != 200:
             raise InvalidLogin()
         return r.json().get('token')
 
     def load_random_song(self):
         r = requests.get(
-            '{}api/songs/random/'.format(BASE_URL),
+            '{}songs/random/'.format(BASE_URL),
             headers={
                 'Authorization': 'TOKEN {}'.format(self.token)
             }
